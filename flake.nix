8984123f9a2ed7b7f61baed1215fed794859a411
Kenichi Kamiya <kachick1@gmail.com>
2024-08-08 03:20:48 +0900

    Refine forAllSystems as nixpkgs do

--- flake.nix
+++ flake.nix
@@ -11,14 +11,12 @@
   outputs =
     { self, nixpkgs }:
     let
-      # Candidates: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/lib/systems/flake-systems.nix
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        # I don't have M1+ mac, providing this for macos-14 free runner https://github.com/actions/runner-images/issues/9741
-        "aarch64-darwin"
-      ];
+      lib = nixpkgs.lib;
+      # List: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/lib/systems/flake-systems.nix
+      #
+      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/flake.nix?plain=1#L11
+      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/lib/systems/default.nix?plain=1#L48-L56
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);

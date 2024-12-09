2f3957c71f82c950846524d736fa6fa00d6c7c31
Kenichi Kamiya <kachick1@gmail.com>
2024-12-10 01:15:07 +0900

    Update Linked ref

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     }:
     let
       inherit (nixpkgs) lib;
-      # List: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/lib/systems/flake-systems.nix
+      # List: https://github.com/NixOS/nixpkgs/blob/nixos-24.11/lib/systems/flake-systems.nix
       #
       # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/flake.nix?plain=1#L11
       # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/lib/systems/default.nix?plain=1#L48-L56
